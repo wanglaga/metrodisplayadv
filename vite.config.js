c131cf81f580f523bd2c25722ef5b9dad214fa52
wanglaga <wanglaga17@gmail.com>
2025-08-27 15:11:41 +0700

    Vite CSS Error 4

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            buildDirectory: 'public/build', // <- folder build di root
         }),
         tailwindcss(),
     ],
-    build: {
-        outDir: 'public/build', // <- folder build akan dibuat di root
-        emptyOutDir: true,
-    },
-});
+})
